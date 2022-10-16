cef0d5e16a4851c594f25e0213f0cb7d3d3ac2e0
Svetlana Drankovich <svetadrankovich55@gmail.com>
2022-10-16 19:36:07 +0300

    My parameterized test

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    private  int age;
+
+
+    public boolean checkIsAdult( int age){
+        if (age >= 18){
+            return true;
+        }return false;
+    }
+}
---
+++ src/test/java/CheckIsAdultTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters // Пометь метод аннотацией для параметров
+    public static Object[][] getTextData() {
+        return new Object[][] {
+
+                {18, true},
+                {19, true},
+                {20, true},
+                {21, true},
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("пользователю должно быть больше 18 лет", true, isAdult );
+    }
+}
